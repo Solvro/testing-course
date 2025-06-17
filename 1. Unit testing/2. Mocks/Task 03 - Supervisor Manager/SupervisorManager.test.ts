4414c4d4fea58cfbe8be4cca45accf6786e049b2
D0dii <marcindolatowski123@gmail.com>
2025-06-17 17:10:23 +0200

    test: check if db.sql is called proper number of times

--- 1. Unit testing/2. Mocks/Task 03 - Supervisor Manager/SupervisorManager.test.ts
+++ 1. Unit testing/2. Mocks/Task 03 - Supervisor Manager/SupervisorManager.test.ts
@@ -1,6 +1,6 @@
 import { describe, expect, it, vi } from "vitest";
 import { db } from "../utils/db";
-import { Supervisor } from "./SupervisorManager";
+import { Supervisor, SupervisorManager } from "./SupervisorManager";
 vi.mock("../utils/db.ts");
 
 const mockedSupervisors: Supervisor[] = [
@@ -16,9 +16,17 @@ const mockedSupervisors: Supervisor[] = [
 
 describe("Supervisor", () => {
   it("Get all supervisors", async () => {
-    vi.setSystemTime(new Date(2025, 5, 13, 14));
+    vi.setSystemTime(new Date(2025, 5, 13, 4));
     vi.mocked(db.sql).mockResolvedValue(mockedSupervisors);
-    const result = await db.sql("elo zelo");
-    expect(result).toBe(mockedSupervisors);
+    const supervisor = new SupervisorManager();
+    await supervisor.addSupervisor({
+      id: "1234",
+      name: "Bartosz",
+      expertiseTopics: ["Elo", "zelo"],
+      rating: 5.0,
+      currentLoad: 10,
+      maxLoad: 999999,
+    });
+    expect(db.sql).toHaveBeenCalledTimes(2);
   });
 });
