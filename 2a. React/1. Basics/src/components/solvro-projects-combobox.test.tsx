7d47ce10b6a54da2ebb80560661f340938f2d649
Wojciech Kosmalski <jawmen999@gmail.com>
2025-06-20 09:19:13 +0200

    refactor: use setup func to use user-event API

--- 2a. React/1. Basics/src/components/solvro-projects-combobox.test.tsx
+++ 2a. React/1. Basics/src/components/solvro-projects-combobox.test.tsx
@@ -7,13 +7,15 @@ describe("SolvroProjectsCombobox", () => {
   const renderComponent = async () => {
     render(<SolvroProjectsCombobox />);
     const combobox = screen.getByRole("combobox");
-    await userEvent.click(combobox);
+    const user = userEvent.setup();
+    await user.click(combobox);
 
     return {
       combobox,
-      searchbox: screen.getByPlaceholderText(/szukaj/i),
+      searchBox: screen.getByPlaceholderText(/szukaj/i),
       group: screen.getByRole("group"),
       getOptions: () => screen.getAllByRole("option"),
+      user,
     };
   };
 
@@ -27,67 +29,67 @@ describe("SolvroProjectsCombobox", () => {
   });
 
   it("should display list of projects and search input after user clicks", async () => {
-    const { searchbox, group } = await renderComponent();
+    const { searchBox: searchbox, group } = await renderComponent();
 
     expect(searchbox).toBeInTheDocument();
     expect(group).toBeInTheDocument();
   });
 
   it("should properly filter projects after typing into searchbox", async () => {
-    const { searchbox, getOptions } = await renderComponent();
+    const { searchBox: searchbox, getOptions, user } = await renderComponent();
 
     //let's assume the data used in component is mocked
-    await userEvent.type(searchbox, "ownik");
+    await user.type(searchbox, "ownik");
 
     expect(getOptions().length).toBe(2);
   });
 
   it("should display informative message when no projects were found", async () => {
-    const { searchbox } = await renderComponent();
+    const { searchBox: searchbox, user } = await renderComponent();
 
-    await userEvent.type(searchbox, "asdf".repeat(10));
+    await user.type(searchbox, "asdf".repeat(10));
 
     expect(screen.getByText(/nie/i)).toBeInTheDocument();
   });
 
   it("should update combobox text after clicking an option", async () => {
-    const { combobox, getOptions } = await renderComponent();
+    const { combobox, getOptions, user } = await renderComponent();
 
     const option = getOptions()[0];
 
-    await userEvent.click(option);
+    await user.click(option);
 
     expect(combobox.textContent).toContain(option.textContent);
   });
 
   it("should update combobox text to default after unclicking an option", async () => {
-    const { combobox, getOptions } = await renderComponent();
+    const { combobox, getOptions, user } = await renderComponent();
 
     const option = getOptions()[0];
 
-    await userEvent.click(option);
+    await user.click(option);
 
     expect(combobox.textContent).toContain(option.textContent);
 
-    await userEvent.click(combobox);
-    await userEvent.click(getOptions()[0]);
+    await user.click(combobox);
+    await user.click(getOptions()[0]);
 
     expect(combobox).toHaveTextContent(/szukaj/i);
   });
 
   it("should update combobox text after clicking different option", async () => {
-    const { combobox, getOptions } = await renderComponent();
+    const { combobox, getOptions, user } = await renderComponent();
 
     const option1 = getOptions()[0];
 
-    await userEvent.click(option1);
+    await user.click(option1);
 
     expect(combobox.textContent).toContain(option1.textContent);
 
-    await userEvent.click(combobox);
+    await user.click(combobox);
 
     const option2 = getOptions()[1];
-    await userEvent.click(option2);
+    await user.click(option2);
 
     expect(combobox.textContent).toContain(option2.textContent);
   });
