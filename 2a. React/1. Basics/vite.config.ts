d9f18140702d7035d1508b4980821982e0279ccd
Jakub Morawiec <jakubmorawiec2@gmail.com>
2025-06-20 21:40:11 +0000

    changed vite config

--- 2a. React/1. Basics/vite.config.ts
+++ 2a. React/1. Basics/vite.config.ts
@@ -1,6 +1,6 @@
 import path from "node:path";
 import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
@@ -8,6 +8,13 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  test: {
+    environment: "jsdom",
+    globals: true,
+    coverage: {
+      reporter: ["text", "html"],
     },
   },
 });
