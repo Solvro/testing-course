44ebafe49b4826a1f42a74e2e976d38e7ddf4598
Konrad Guzek <konrad@guzek.uk>
2025-06-24 17:34:18 +0200

    test: test loading state

--- 2a. React/2. Mocking/src/components/solvro-projects-combobox-api.test.tsx
+++ 2a. React/2. Mocking/src/components/solvro-projects-combobox-api.test.tsx
@@ -1,11 +1,15 @@
 import { it, expect, describe } from "vitest";
-import { render, screen } from "@testing-library/react";
+import {
+  render,
+  screen,
+  waitForElementToBeRemoved,
+} from "@testing-library/react";
 import { SolvroProjectsComboboxApi } from "./solvro-projects-combobox-api";
 import "@testing-library/jest-dom/vitest";
 import { userEvent } from "@testing-library/user-event";
 import { API_PROJECTS_URL, MOCK_PROJECTS } from "@/tests/mocks/handlers";
 import { server } from "@/tests/mocks/server";
-import { http, HttpResponse } from "msw";
+import { delay, http, HttpResponse } from "msw";
 import { Providers } from "./providers";
 
 function getCombobox() {
@@ -25,7 +29,37 @@ async function clickCombobox() {
   return user;
 }
 
+const getLoader = () => screen.queryByText(/Ładowanie projektów.../i);
+
 describe("SolvroProjectsComboboxApi", () => {
+  it("should render an error message when the API fails", async () => {
+    server.use(
+      http.get(API_PROJECTS_URL, () => HttpResponse.json({}, { status: 500 })),
+    );
+    await clickCombobox();
+    const list = screen.getByRole("listbox");
+    expect(list).toHaveTextContent("Błąd podczas ładowania projektów");
+  });
+
+  it("should show a loading state while fetching projects", async () => {
+    server.use(
+      http.get(API_PROJECTS_URL, async () => {
+        await delay(1000);
+        return HttpResponse.json({ projects: MOCK_PROJECTS });
+      }),
+    );
+    await clickCombobox();
+    expect(screen.getByRole("listbox")).toHaveTextContent(
+      "Ładowanie projektów...",
+    );
+    expect(getLoader()).toBeInTheDocument();
+  });
+
+  it("should remove loading state after projects are loaded", async () => {
+    await clickCombobox();
+    await waitForElementToBeRemoved(getLoader);
+  });
+
   it("should render a select prompt", () => {
     const combobox = getCombobox();
     expect(combobox).toBeInTheDocument();
@@ -39,14 +73,5 @@ describe("SolvroProjectsComboboxApi", () => {
     list.childNodes.forEach((option, index) => {
       expect(option).toHaveTextContent(MOCK_PROJECTS[index].label);
     });
-  });
-
-  it("should render an error message when the API fails", async () => {
-    server.use(
-      http.get(API_PROJECTS_URL, () => HttpResponse.json({}, { status: 500 })),
-    );
-    await clickCombobox();
-    const list = screen.getByRole("listbox");
-    expect(list).toHaveTextContent("Błąd podczas ładowania projektów");
   });
 });
