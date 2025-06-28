fe8d78b0a28dc2829a6f4c034cb3132ffbc4bb31
Jakub Czajkowski <and.jj.an@gmail.com>
2025-06-29 00:33:48 +0200

    fix: import afterEach from vitest for proper test cleanup

--- 2a. React/2. Mocking/src/tests/setup.ts
+++ 2a. React/2. Mocking/src/tests/setup.ts
@@ -1,8 +1,7 @@
-import { afterAll, beforeAll, vi } from "vitest";
+import { afterAll, afterEach, beforeAll, vi } from "vitest";
 import "@testing-library/jest-dom/vitest";
 import ResizeObserver from "resize-observer-polyfill";
 import { server } from "./mocks/server";
-import { afterEach } from "node:test";
 
 beforeAll(() => server.listen());
 afterEach(() => server.resetHandlers());
