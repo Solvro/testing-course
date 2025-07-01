e31aadf9ab2149ebfe79a9e8debd812beed898d7
Wojciech Kosmalski <jawmen999@gmail.com>
2025-07-01 20:06:55 +0200

    feat: add tests for LoginPage and router

--- 2a. React/3. Advanced tests/src/pages/login.test.tsx
+++ 2a. React/3. Advanced tests/src/pages/login.test.tsx
@@ -1,25 +1,75 @@
-import { describe, it } from "vitest";
-import { render } from "@testing-library/react";
+import { describe, expect, it } from "vitest";
+import { render, screen } from "@testing-library/react";
 import { LoginPage } from "@/pages/login";
 import { Providers } from "@/components/providers";
 import { userEvent } from "@testing-library/user-event";
+import {
+  MOCK_EMAIL,
+  MOCK_OTP,
+  MOCK_OTP_INVALID,
+} from "@/tests/mocks/constants";
 
-// To jest tylko przykładowy test, żeby łatwiej wam było zacząć - możecie go usunąć lub zmodyfikować
 describe("Login Page", () => {
-  it("should validate my email", async () => {
-    const screen = render(<LoginPage />, {
+  function renderComponent() {
+    render(<LoginPage />, {
       wrapper: Providers,
     });
     const user = userEvent.setup();
 
-    const emailInput = screen.getByLabelText("Adres e-mail");
+    return {
+      user,
+      enterEmail: async (email: string) => {
+        const emailInput = screen.getByLabelText("Adres e-mail");
+        await user.type(emailInput, email);
+      },
+      enterOtp: async (otp: string) => {
+        const inputOtp = screen.getByRole("textbox");
+        await user.type(inputOtp, otp);
+      },
+      clickSubmitButton: async () => {
+        const button = screen.getByRole("button");
+        await user.click(button);
+      },
+    };
+  }
 
-    const submitButton = screen.getByRole("button");
+  it("should validate an email", async () => {
+    const { enterEmail, clickSubmitButton } = renderComponent();
 
-    await user.type(emailInput, "invalid-email");
-    await user.click(submitButton);
+    await enterEmail("invalid-email");
+    await clickSubmitButton();
 
-    // Co dalej?
-    screen.debug();
+    expect(screen.getByText(/poprawny/i)).toBeInTheDocument();
+  });
+
+  it("should navigate to otp step when passing correct email", async () => {
+    const { enterEmail, clickSubmitButton } = renderComponent();
+
+    await enterEmail(MOCK_EMAIL);
+    await clickSubmitButton();
+
+    expect(screen.getByText(/hasło/i)).toBeInTheDocument();
+  });
+
+  it("should display toast with success message when typing correct OTP", async () => {
+    const { enterEmail, clickSubmitButton, enterOtp } = renderComponent();
+    await enterEmail(MOCK_EMAIL);
+    await clickSubmitButton();
+
+    await enterOtp(MOCK_OTP);
+    await clickSubmitButton();
+
+    expect(screen.getByText(/zalogowano pomyślnie/i)).toBeInTheDocument();
+  });
+
+  it("should display toast with failure message when typing incorrect OTP", async () => {
+    const { enterEmail, clickSubmitButton, enterOtp } = renderComponent();
+    await enterEmail(MOCK_EMAIL);
+    await clickSubmitButton();
+
+    await enterOtp(MOCK_OTP_INVALID);
+    await clickSubmitButton();
+
+    expect(screen.getByText(/invalid/i)).toBeInTheDocument();
   });
 });
---
+++ 2a. React/3. Advanced tests/src/tests/mocks/constants.ts
@@ -0,0 +1,3 @@
+export const MOCK_OTP = "123456";
+export const MOCK_OTP_INVALID = "010101";
+export const MOCK_EMAIL = "test@student.pwr.edu.pl";
