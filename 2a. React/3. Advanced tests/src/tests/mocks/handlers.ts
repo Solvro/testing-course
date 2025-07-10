0707e8fd724feaeea8fd9364e5e836fa66ad0b4b
D0dii <marcindolatowski123@gmail.com>
2025-07-10 20:48:23 +0200

    feat: tests

--- 2a. React/3. Advanced tests/src/tests/mocks/handlers.ts
+++ 2a. React/3. Advanced tests/src/tests/mocks/handlers.ts
@@ -1,3 +1,16 @@
-import type { RequestHandler } from "msw";
+import { BASE_URL } from "@/api/base-url";
+import { http, HttpResponse } from "msw";
 
-export const handlers = [] satisfies RequestHandler[];
+export const handlers = [
+  http.post(`${BASE_URL}/user/otp/get`, async () => {
+    return HttpResponse.json({
+      otp: "123456",
+    });
+  }),
+  http.post(`${BASE_URL}/user/otp/verify`, async () => {
+    return HttpResponse.json({
+      succes: true,
+      message: "Login successful",
+    });
+  }),
+];
