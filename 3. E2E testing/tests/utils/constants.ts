9442a10c5dd5ec0afde4dd13071c84f3e44f6c9a
olekszczepanowski <olekszczepanowski04@wp.pl>
2025-07-15 19:37:27 +0200

    feat: tests

---
+++ 3. E2E testing/tests/utils/constants.ts
@@ -0,0 +1,4 @@
+export const BASE_URL = "http://localhost:5173/";
+export const VALID_EMAIL = "13123@student.pwr.edu.pl";
+export const INVALID_EMAIL = "dodidodi";
+export const INVALID_EMAIL_DOMAIN = "dodidodo@xd.pl";
