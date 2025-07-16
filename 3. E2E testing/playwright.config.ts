40442035310457ee1065c4777bd09f6e1407f742
Konrad Guzek <konrad@guzek.uk>
2025-07-16 21:37:35 +0200

    chore: stylistic fixes

--- 3. E2E testing/playwright.config.ts
+++ 3. E2E testing/playwright.config.ts
@@ -56,7 +56,7 @@ export default defineConfig({
     // {
     //   name: 'Mobile Chrome',
     //   use: { ...devices['Pixel 5'] },
-    // },|
+    // },
     // {
     //   name: 'Mobile Safari',
     //   use: { ...devices['iPhone 12'] },
